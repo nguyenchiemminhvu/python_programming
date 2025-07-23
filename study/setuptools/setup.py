bee6aba371633689d49dee87151d9901bab84135
vu3.nguyen <vu3.nguyen@lge.com>
2025-07-23 14:22:53 +0900

    examples of setuptools

---
+++ study/setuptools/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sample_package',
+    version='0.1.0',
+    description='An example package to demonstrate setuptools usage',
+    author='Vu Nguyen',
+    author_email="nguyenchiemminhvu@gmail.com",
+    packages=["setuptools_example"],
+    install_requires=[
+    ],
+)
---
+++ study/setuptools/setuptools_example/__main__.py
@@ -0,0 +1,5 @@
+def main():
+    print("This is the main function of the setuptools example package.")
+
+if __name__ == "__main__":
+    main()
