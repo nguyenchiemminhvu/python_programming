3d4a3176dc6a807a22f42432de1746ba8af456a8
vu.nguyenchiemminh <nguyenchiemminhvu@gmail.com>
2025-07-26 21:29:30 +0700

    update

---
+++ study/basis/boolean.py
@@ -0,0 +1,20 @@
+print(10==10) # True
+print(10==20) # False
+print(10!=20) # True
+print(10!=10) # False
+print(10>20)  # False
+print(10<20)  # True
+print(10>=10) # True
+print(10>=20) # False
+print(10<=10) # True
+print(10<=20) # True
+print(10>10)  # False
+print(10<10)  # False
+print(10>=5)  # True
+print(10<=5)  # False
+print(bool("")) # False
+print(bool(" ")) # True
+print(bool([])) # False
+print(bool([1, 2, 3])) # True
+print(bool(None)) # False
+print(bool(not None)) # True
