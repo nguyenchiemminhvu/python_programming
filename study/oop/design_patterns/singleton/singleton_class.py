4954d312e78b2bddfdfc4f69a248d64a30125c7e
vu.nguyenchiemminh <nguyenchiemminhvu@gmail.com>
2025-08-15 01:12:12 +0700

    udpate

--- study/oop/design_patterns/singleton/singleton_class.py
+++ study/oop/design_patterns/singleton/singleton_class.py
@@ -10,7 +10,11 @@ class SingletonChild(Singleton):
     def __init__(self):
         super().__init__()
         self.some_property = "I am a property of SingletonChild"
-    
+
+class SingletonChild2(Singleton):
+    def __init__(self):
+        super().__init__()
+        self.another_property = "I am a property of SingletonChild2"
 
 if __name__ == "__main__":
     s1 = SingletonChild()
@@ -18,3 +22,11 @@ if __name__ == "__main__":
     print(s1 is s2)  # Should print True, proving both are the same instance
     s1.some_property = "Changed property"
     print(s2.some_property)
+    
+    s3 = SingletonChild2()
+    s4 = SingletonChild2()
+    print(s3 is s4)  # Should print True, proving both are the same instance
+    s4.another_property = "Changed property"
+    print(s3.another_property)
+    
+    print(s1 is s3)  # Should print False, proving they are different singleton instances
--- study/oop/design_patterns/singleton/singleton_meta.py
+++ study/oop/design_patterns/singleton/singleton_meta.py
@@ -1,15 +1,20 @@
 class Singleton(type):
     __instance = None
     
-    def __call__(self, *args, **kwds):
-        if not self.__instance:
-            self.__instance = super().__call__(*args, **kwds)
-        return self.__instance
+    def __call__(cls, *args, **kwds):
+        if not cls.__instance:
+            cls.__instance = super().__call__(*args, **kwds) # call the type.__new__ and __init__
+        return cls.__instance
 
 class SingletonClass(metaclass=Singleton):
     def __init__(self):
         super().__init__()
         self.value = 42
+
+class SingletonClass2(metaclass=Singleton):
+    def __init__(self):
+        super().__init__()
+        self.another_value = 84
 
 if __name__ == "__main__":
     s1 = SingletonClass()
@@ -17,3 +22,11 @@ if __name__ == "__main__":
     print(s1 is s2)
     s1.value = 100
     print(s2.value)
+    
+    s3 = SingletonClass2()
+    s4 = SingletonClass2()
+    print(s3 is s4)
+    s4.another_value = 200
+    print(s3.another_value)
+    
+    print(s1 is s3)  # Should be False, different singleton classes
