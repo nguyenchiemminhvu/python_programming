751df86cb8d44fb61298f31f6d34361005fe67d1
vu3.nguyen <vu3.nguyen@lge.com>
2025-08-25 18:57:15 +0900

    update

--- study/oop/design_patterns/state/basic_state.py
+++ study/oop/design_patterns/state/basic_state.py
@@ -1,40 +1,119 @@
 from abc import ABC, abstractmethod
 import time
 
-class context(ABC):
+class media_state(ABC):
+    def __init__(self, media_player):
+        self.media_player = media_player
+    
     @abstractmethod
-    def change_state(self, s : "state"):
+    def play(self):
+        pass
+    
+    @abstractmethod
+    def pause(self):
+        pass
+    
+    @abstractmethod
+    def next(self):
+        pass
+    
+    @abstractmethod
+    def prev(self):
         pass
 
-class state(ABC):
-    @abstractmethod
-    def operating(self, c : context):
-        pass
+class playing_state(media_state):
+    def __init__(self, media_player):
+        super().__init__(media_player)
 
-class concrete_state_A(state):
-    def operating(self, c: context):
-        print("State A is operating.")
+    def play(self):
+        print("Already playing.")
+    
+    def pause(self):
+        print("Pausing playback.")
+        self.media_player.set_state(self.media_player.paused_state)
+    
+    def next(self):
+        self.media_player.next_track()
+        self.media_player.play_current_track()
+    
+    def prev(self):
+        self.media_player.prev_track()
+        self.media_player.play_current_track()
 
-class concrete_state_B(state):
-    def operating(self, c: context):
-        print("State B is operating.")
+class paused_state(media_state):
+    def __init__(self, media_player):
+        super().__init__(media_player)
+    
+    def play(self):
+        print("Resuming playback.")
+        self.media_player.set_state(self.media_player.playing_state)
+        self.media_player.play_current_track()
+    
+    def pause(self):
+        print("Already paused.")
+    
+    def next(self):
+        self.media_player.next_track()
+        self.media_player.set_state(self.media_player.playing_state)
+        self.media_player.play_current_track()
+    
+    def prev(self):
+        self.media_player.prev_track()
+        self.media_player.set_state(self.media_player.playing_state)
+        self.media_player.play_current_track()
 
-class concrete_context(context):
-    def __init__(self):
-        self.state = None
+class media_player:
+    def __init__(self, playlist):
+        self.playlist = playlist
+        self.current_index = 0
+        
+        self.playing_state = playing_state(self)
+        self.paused_state = paused_state(self)
+        
+        self.state = self.paused_state
     
-    def change_state(self, s: state):
-        self.state = s
+    def set_state(self, state):
+        self.state = state
+        print(f"State changed to: {type(self.state).__name__}")
     
-    def operate(self):
-        if self.state is not None:
-            self.state.operating(self)
+    def current_track(self):
+        return self.playlist[self.current_index]
     
+    def next_track(self):
+        self.current_index = (self.current_index + 1) % len(self.playlist)
+    
+    def prev_track(self):
+        self.current_index = (self.current_index - 1) % len(self.playlist)
+    
+    def play(self):
+        self.state.play()
+    
+    def pause(self):
+        self.state.pause()
+    
+    def next(self):
+        self.state.next()
+    
+    def prev(self):
+        self.state.prev()
+    
+    def play_current_track(self):
+        print(f"Playing: {self.current_track()}")
+
 if __name__ == "__main__":
-    c = concrete_context()
-    c.change_state(concrete_state_A())
-    c.operate()
+    playlist = ["Song 1", "Song 2", "Song 3"]
+    player = media_player(playlist)
+
+    player.play()  # Should start playing Song 1
     time.sleep(1)
-    c.change_state(concrete_state_B())
-    c.operate()
-    c.operate()
+    player.next()  # Should go to Song 2
+    time.sleep(1)
+    player.pause() # Should pause playback
+    time.sleep(1)
+    player.play()  # Should resume playback
+    time.sleep(1)
+    player.prev()  # Should go back to Song 1
+    time.sleep(1)
+    player.pause() # Should pause playback
+    time.sleep(1)
+    player.next()  # Should go to Song 2 and play
